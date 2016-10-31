1f0ae9a7451560eb60662ff0ecf21fd6fc0e08a7
smartools <gacelyc@gmail.com>
2016-10-30 19:27:17 -0500

    no message

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,7 +3,7 @@
 # in the file  aws.yml or aws.rb
 
 AWS.config({
-	:access_key_id => 'AKIAJBCDCBBWFRJFVWVA',
-	:secret_access_key => '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'
+	:access_key_id => '',
+	:secret_access_key => ''
 })
 
