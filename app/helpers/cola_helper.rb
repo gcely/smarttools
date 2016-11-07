97d051249874947f99a51a6e15a468c6eaea4dab
smartools <gacelyc@gmail.com>
2016-11-06 22:54:10 -0500

    no message

--- app/helpers/cola_helper.rb
+++ app/helpers/cola_helper.rb
@@ -2,11 +2,11 @@ require 'aws-sdk'
 
 module ColaHelper
 	def send_msg_to_queue(message)
-		client = IronMQ::Client.new(host: 'mq-aws-eu-west-1-1.iron.io',
-									token: '78lTkar7aFpExwRfr2Vp',
-									project_id: '581fed5fbccbd80006c37c72')
-		queue = client.queue("smarttools_queue")
-		resp = queue.post('mensaje ')
+		@client = IronMQ::Client.new(host: ENV['IRON_QUE_HOST'],
+									token: ENV['IRON_QUE_TOKEN'],
+									project_id: ENV['IRON_QUE_ID'])
+		@queue = @client.queue("smarttools_queue")
+		resp = @queue.post('mensaje ')
 	end
 	def obtain_message_from_queue
 		client = IronMQ::Client.new(host: ENV['IRON_QUE_HOST'],
