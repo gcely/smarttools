86f43702cc6094c8e64bc8156e35d951c7babf0d
smartools <gacelyc@gmail.com>
2016-11-07 01:35:14 -0500

    no message

--- app/helpers/cola_helper.rb
+++ app/helpers/cola_helper.rb
@@ -14,7 +14,7 @@ module ColaHelper
 									project_id: ENV['IRON_QUE_ID'])
 		queue = client.queue("smarttools_queue")
 		resp = queue.get()
-		puts resp
+		puts resp.body
 		return resp.body
 	end
 	def delete_message_from_queue(receipt_handle)
