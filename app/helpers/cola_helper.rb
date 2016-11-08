45bb47895ce19c462060aef128f454e9f736171a
smartools <gacelyc@gmail.com>
2016-11-07 23:35:39 -0500

    no message

--- app/helpers/cola_helper.rb
+++ app/helpers/cola_helper.rb
@@ -2,25 +2,25 @@ require 'aws-sdk'
 
 module ColaHelper
 	def send_msg_to_queue(message)
-		client = IronMQ::Client.new(host: ENV['IRON_QUE_HOST'],
-									token: ENV['IRON_QUE_TOKEN'],
-									project_id: ENV['IRON_QUE_ID'])
+		client = IronMQ::Client.new(host: ENV['IRON_MQ_HOST'],
+									token: ENV['IRON_MQ_TOKEN'],
+									project_id: ENV['IRON_MQ_PROJECT_ID'])
 		queue = client.queue("smarttools_queue")
 		resp = queue.post(message)
 	end
 	def obtain_message_from_queue
-		client = IronMQ::Client.new(host: ENV['IRON_QUE_HOST'],
-									token: ENV['IRON_QUE_TOKEN'],
-									project_id: ENV['IRON_QUE_ID'])
+		client = IronMQ::Client.new(host: ENV['IRON_MQ_HOST'],
+									token: ENV['IRON_MQ_TOKEN'],
+									project_id: ENV['IRON_MQ_PROJECT_ID'])
 		queue = client.queue("smarttools_queue")
 		resp = queue.reserve(n: 1)
 		puts resp.body
 		return resp.body
 	end
 	def delete_message_from_queue(receipt_handle)
-		client = IronMQ::Client.new(host: ENV['IRON_QUE_HOST'],
-									token: ENV['IRON_QUE_TOKEN'],
-									project_id: ENV['IRON_QUE_ID'])
+		client = IronMQ::Client.new(host: ENV['IRON_MQ_HOST'],
+									token: ENV['IRON_MQ_TOKEN'],
+									project_id: ENV['IRON_MQ_PROJECT_ID'])
 		queue = client.queue("smarttools_queue")
 		resp = queue.delete_reserved_messages(receipt_handle)
 	end	
