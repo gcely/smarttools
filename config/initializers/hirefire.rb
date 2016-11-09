ad3e6633816fa046d5a3977698ce072d8ea8b109
smartools <gacelyc@gmail.com>
2016-11-08 23:04:44 -0500

    no message

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,7 +1,7 @@
-client = IronMQ::Client.new(host: ENV['IRON_MQ_HOST'],
-									token: ENV['IRON_MQ_TOKEN'],
-									project_id: ENV['IRON_MQ_PROJECT_ID'])
-	queue = client.queue("smarttools_queue")
+#client = IronMQ::Client.new(host: ENV['IRON_MQ_HOST'],
+#									token: ENV['IRON_MQ_TOKEN'],
+#									project_id: ENV['IRON_MQ_PROJECT_ID'])
+#	queue = client.queue("smarttools_queue")
 
 
 HireFire::Resource.configure do |config|
